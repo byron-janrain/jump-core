f2dad3852aa5e730e135c7aebef093e6c3e9545f
Byron Kam <byron@janrain.com>
2013-05-21 16:14:27 -0700

    removed ref

--- lib/janrain/plex/AbstractConfig.php
+++ lib/janrain/plex/AbstractConfig.php
@@ -1,9 +1,11 @@
 <?php
 namespace janrain\plex;
-
+/**
+ * 
+ */
 abstract class AbstractConfig extends \ArrayObject implements ConfigInterface
 {
-	public function __construct(Array $data, Array &$required)
+	public function __construct(Array $data, Array $required)
 	{
 		foreach ($required as &$key) {
 			if (array_key_exists($key, $data)) {
