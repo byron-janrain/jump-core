e2ac54ecc27947f1341179d1ecc593d043b4267d
Byron Kam <byron@janrain.com>
2013-05-21 16:26:00 -0700

    remove refs

--- lib/janrain/plex/AbstractConfig.php
+++ lib/janrain/plex/AbstractConfig.php
@@ -7,12 +7,12 @@ abstract class AbstractConfig extends \ArrayObject implements ConfigInterface
 {
 	public function __construct(Array $data, Array $required)
 	{
-		foreach ($required as &$key) {
+		foreach ($required as $key) {
 			if (array_key_exists($key, $data)) {
 				$this[$required] = $data[$required];				
 			} else {
 				throw new \InvalidArgumentException("Required config {$key} not found!");
 			}
 		}
-	}	
+	}
 }
