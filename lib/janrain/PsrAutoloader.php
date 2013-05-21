cffb94fcaf77a11e7ea2f1ac713e7ee1582c177b
Byron Kam <byron@janrain.com>
2013-05-21 16:02:40 -0700

    moved autoloader

--- lib/janrain/PsrAutoloader.php
+++ lib/janrain/PsrAutoloader.php
@@ -3,6 +3,6 @@ spl_autoload_register(
 	function ($className) {
 		if ('janrain' == strstr($className, '\\', true)) {
 			$className = str_replace('\\', '/', $className);
-			require_once dirname(dirname(__DIR__)) . "/{$className}.php";
+			require_once dirname(__DIR__) . "/{$className}.php";
 		}
 	}, true, true);
