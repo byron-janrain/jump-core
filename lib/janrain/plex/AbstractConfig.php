8435d545b7d76eb764de8eb931e2ca2073405f86
Byron Kam <byron@janrain.com>
2013-05-21 16:34:22 -0700

    fixed key reference

--- lib/janrain/plex/AbstractConfig.php
+++ lib/janrain/plex/AbstractConfig.php
@@ -5,11 +5,11 @@ namespace janrain\plex;
  */
 abstract class AbstractConfig extends \ArrayObject implements ConfigInterface
 {
-	public function __construct(Array $data, Array $required)
+	public function __construct(Array $data, Array &$required)
 	{
-		foreach ($required as $key) {
+		foreach ($required as &$key) {
 			if (array_key_exists($key, $data)) {
-				$this[$required] = $data[$required];				
+				$this[$key] = $data[$key];
 			} else {
 				throw new \InvalidArgumentException("Required config {$key} not found!");
 			}
