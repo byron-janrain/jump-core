cb7f737b5f628a4fbb195fd20e11ce0383f3f385
Byron Kam <byron@janrain.com>
2013-05-21 15:42:07 -0700

    add autoloader

---
+++ lib/janrain/jump/PsrAutoloader.php
@@ -0,0 +1,7 @@
+<?php
+spl_autoload_register(
+	function ($className) {
+		if ('janrain' == strstr($className, '\\', true)) {
+			require_once dirname(__DIR__) . '/../' . str_replace('\\', '/', $className) . '.php';
+		}
+	}, true, true);
