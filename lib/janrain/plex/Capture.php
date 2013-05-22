40808826df496dd7b7fc210411efe54dd8dcecdc
Byron Kam <byron@janrain.com>
2013-05-22 16:37:58 -0700

    fixed prototype bad .map implementation
    also cleaned up some formatting

--- lib/janrain/plex/Capture.php
+++ lib/janrain/plex/Capture.php
@@ -1,70 +1,110 @@
 <?php
 namespace janrain\plex;
 
-class Capture implements RenderableInterface {
-
+class Capture implements RenderableInterface
+{
     protected $config;
 
-	public function __construct(CaptureConfigInterface $config) {
+    public function __construct(CaptureConfigInterface $config)
+    {
         $this->config = $config;
-	}
+    }
 
-	public function getJsSrcs() {
-		return array();//'https://d7v0k4dt27zlp.cloudfront.net/assets/capture_client.js');
-	}
+    public function getJsSrcs()
+    {
+        return array();//'https://d7v0k4dt27zlp.cloudfront.net/assets/capture_client.js');
+    }
 
-	public function getStartHeadJs() {
-		$out = "(function(){
-            window.janrain = { settings: { packages: [], capture: {}}};
-			// if (typeof window.janrain !== 'object') window.janrain = {};
-			// if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
-			// if (typeof window.janrain.settings.packages !== 'object') window.janrain.settings.packages = [];
-			// if (typeof window.janrain.settings.capture !== 'object') window.janrain.settings.capture = {};
-            janrain.settings.packages.push('capture');\n";
-		return $out;
-	}
+    public function getStartHeadJs()
+    {
+        $out = "(function(){
+            var opts;
+            if (typeof window.janrain !== 'object') {window.janrain = {};}
+            if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
+            opts = window.janrain.settings;
+            if (typeof opts.packages !== 'object') opts.packages = [];
+            if (typeof opts.capture !== 'object') opts.capture = {};
+            opts.packages.push('capture');\n";
+        $out .=
+            "if (typeof Prototype !== 'undefined') {
+                var protVer = 0;
+                var m = 100;
+                Prototype.Version.split('.').forEach(
+                    function (value) {
+                        protVer += m * value;
+                        m /= 10;
+                    });
+                if (protVer < 171) {
+                    //FIX PROTOTYPE
+                    Array.prototype.map = function(callback, thisArg) {
+                        var T, A, k;
+                        if (this == null) throw new TypeError(' this is null or not defined');
+                        var O = Object(this);
+                        var len = O.length >>> 0;
+                        if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+                        if (thisArg) T = thisArg;
+                        A = new Array(len);
+                        k = 0;
+                        while (k < len) {
+                            var kValue, mappedValue;
+                            if (k in O) {
+                                kValue = O[ k ];
+                                mappedValue = callback.call(T, kValue, k, O);
+                                A[ k ] = mappedValue;
+                            }
+                            k++;
+                        }
+                        return A;
+                    };
+                }
+            }\n";
+        return $out;
+    }
 
-	public function getSettingsHeadJs() {
-		$out = "\n//Start Janrain Settings
-			janrain.settings.capture.appId = '{$this->config['capture.appId']}';
-			janrain.settings.capture.clientId = '{$this->config['capture.clientId']}';
-			janrain.settings.capture.captureServer = '{$this->config['capture.captureServer']}';
-			janrain.settings.capture.recaptchaPublicKey = '6LeVKb4SAAAAAGv-hg5i6gtiOV4XrLuCDsJOnYoP';
-			janrain.settings.capture.redirectUri = document.location.href;
-			//janrain.settings.capture.loadJsUrl = 'd16s8pqtk4uodx.cloudfront.net/\$this->engageName/load.js';
-            janrain.settings.capture.loadJsUrl = '{$this->config['capture.loadJsUrl']}';
-			janrain.settings.capture.flowName = 'plugins';
-			janrain.settings.capture.registerFlow = 'socialRegistration';
-			janrain.settings.capture.responseType = 'token';
-			janrain.settings.tokenUrl = '{$this->config['tokenUrl']}';
-			janrain.settings.tokenAction = 'event';
-			//End Janrain Settings\n";
-		return $out;
-	}
+    public function getSettingsHeadJs()
+    {
+        $out = "\n//Start Janrain Settings
+            opts.capture.appId = '{$this->config['capture.appId']}';
+            opts.capture.clientId = '{$this->config['capture.clientId']}';
+            opts.capture.captureServer = '{$this->config['capture.captureServer']}';
+            opts.capture.recaptchaPublicKey = '6LeVKb4SAAAAAGv-hg5i6gtiOV4XrLuCDsJOnYoP';
+            opts.capture.redirectUri = document.location.href;
+            //opts.capture.loadJsUrl = 'd16s8pqtk4uodx.cloudfront.net/\$this->engageName/load.js';
+            //opts.capture.loadJsUrl = '{$this->config['capture.loadJsUrl']}';
+            opts.capture.loadJsUrl = 'd16s8pqtk4uodx.cloudfront.net/default/load.js';
+            opts.capture.flowName = 'plugins';
+            opts.capture.registerFlow = 'signIn';
+            opts.capture.responseType = 'token';
+            opts.tokenUrl = document.location.href;//'{$this->config['tokenUrl']}';
+            opts.tokenAction = 'event';
+            //End Janrain Settings\n";
+        return $out;
+    }
 
-	public function getEndHeadJs() {
-		$out =
-			"function isReady() {janrain.ready=true;}
-			if (document.addEventListener) {
-				document.addEventListener('DOMContentLoaded', isReady, false);
-			} else {
-				window.attachEvent('onload', isReady);
-			}
-			var e = document.createElement('script');
-			e.type = 'text/javascript';
-			e.id = 'janrainAuthWidget';
-			e.src = '//{$this->config['capture.loadJsUrl']}';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(e, s);
-			})();
-			</script>
-			<script type='text/javascript'>
-			function janrainCaptureWidgetOnLoad() {
-				janrain.events.onCaptureLoginSuccess.addHandler(
-					function (result) {
-						alert('Success: this is your token ' + result.accessToken);
+    public function getEndHeadJs()
+    {
+        $out =
+            "function isReady() {janrain.ready=true;}
+            if (document.addEventListener) {
+                document.addEventListener('DOMContentLoaded', isReady, false);
+            } else {
+                window.attachEvent('onload', isReady);
+            }
+            var e = document.createElement('script');
+            e.type = 'text/javascript';
+            e.id = 'janrainAuthWidget';
+            e.src = '//{$this->config['capture.loadJsUrl']}';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(e, s);
+            })();
+            </script>
+            <script type='text/javascript'>
+            function janrainCaptureWidgetOnLoad() {
+                janrain.events.onCaptureLoginSuccess.addHandler(
+                    function (result) {
+                        alert('Success: this is your token ' + result.accessToken);
                         janrain.capture.ui.modal.close();
-					});
+                    });
                 janrain.events.onCaptureLoginFailed.addHandler(
                     function () {
                         console.log('Sign in failure!');
@@ -75,21 +115,27 @@ class Capture implements RenderableInterface {
                         console.log('Registration Success!');
                         janrain.capture.ui.modal.close();
                     });
-				janrain.capture.ui.start();
- 			}\n";
-		return $out;
-	}
-	public function getCssHrefs() {
-		return array(
-			'//d3hmp0045zy3cs.cloudfront.net/2.1.10/quilt.css',
-			'//d3hmp0045zy3cs.cloudfront.net/2.1.10/widgets.css',
-			);
-	}
-	public function getCss() {
-		return file_get_contents(__DIR__ . '/../plex/styles.css');
-	}
-	public function getHtml() {
+                janrain.capture.ui.start();
+            }\n";
+        return $out;
+    }
+
+    public function getCssHrefs()
+    {
+        return array(
+            '//d3hmp0045zy3cs.cloudfront.net/2.1.10/quilt.css',
+            '//d3hmp0045zy3cs.cloudfront.net/2.1.10/widgets.css',
+            );
+    }
+
+    public function getCss()
+    {
+        return file_get_contents(__DIR__ . '/../plex/styles.css');
+    }
+
+    public function getHtml()
+    {
         $screens = file_get_contents(__DIR__ . '/../plex/screens.html');
-		return "<a href='#' class='capture_modal_open'>Sign In</a>\n{$screens}\n";
-	}
+        return "<a href='#' class='capture_modal_open'>Sign In</a>\n{$screens}\n";
+    }
 }
