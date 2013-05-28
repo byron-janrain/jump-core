bcbdddb28e41e45bbf15dbe9004f655cb28545b5
Byron Kam <byron@janrain.com>
2013-05-28 14:56:30 -0700

    Token authorized user fetching
    First draft of using tokens to make api calls.

--- lib/janrain/jump/Api.php
+++ lib/janrain/jump/Api.php
@@ -14,7 +14,7 @@ class Api
 
 	public function __construct(ArrayAccess $opts) {
 		$this->clientId = $opts['capture.clientId'];
-		$this->clientSecret = $opts['capture.clientSecret'];
+		$this->clientSecret = @$opts['capture.clientSecret'];
 		$this->data = array(
 			'client_id' => $this->clientId,
 			'client_secret' => $this->clientSecret,
@@ -22,7 +22,7 @@ class Api
 		$this->ctx = array(
 			'http' => array(
 				'method' => 'POST',
-				'header' => 'Content-type: application/x-www-form-urlencoded',
+				'header' => "Accept-Encoding: identity\r\nContent-type: application/x-www-form-urlencoded\r\n",
 				));
 		$this->url = $opts['capture.captureServer'];
 		if ($this->url[(strlen($this->url) - 1)] !== '/') {
@@ -32,13 +32,27 @@ class Api
 
 	public function __invoke($url, $params) {
 		$params = array_merge($this->data, $params);
+		if (!empty($params['token'])) {
+			unset($params['client_id']);
+			unset($params['client_secret']);
+			$this->ctx['http']['header'] .= "Authorization: OAuth {$params['token']}\r\n";
+			unset($params['token']);
+		}
 		$this->ctx['http']['content'] = http_build_query($params);
 		$stream = stream_context_create($this->ctx);
-		return json_decode(file_get_contents($this->url . $url, false, $stream));
+		$resp = json_decode(file_get_contents($this->url . $url, false, $stream), true);
+		if (empty($resp['stat']) || $resp['stat'] == 'error') {
+			throw new \Exception($resp['error_description']);
+		}
+		return $resp['result'];
 	}
 
-	public function fetchUserBy()
+	public function fetchUserByUuid($uuid, $token = null)
 	{
-
+		echo '<pre>';
+		$data = $this('entity', array('uuid' => $uuid, 'token' => $token, 'type_name'=>'user'));
+		$user = User::__set_state($data);
+		var_dump($user);
+		return $user;
 	}
 }
