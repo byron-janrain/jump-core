c8d5cbf4828642968c25cc687dff4a9e1b38137b
Byron Kam <byron@janrain.com>
2013-07-12 16:45:59 -0700

    jump updates
    jump can now be insantiated without config data.  has a convenient
    render function to do a full dump

--- lib/janrain/Jump.php
+++ lib/janrain/Jump.php
@@ -7,22 +7,6 @@ final class Jump implements plex\RenderableInterface
 {
 	private $features;
 
-	private function __construct($data)
-	{
-		$this->features = new plex\FeatureStack();
-		$coreConfig = jump\ConfigBuilder::build('janrain\\plex\\Core', $data);
-		$core = new plex\Core($coreConfig);
-		$this->features->pushFeature($core);
-		foreach ($data['features'] as $fName) {
-			$fClass = '\\janrain\\plex\\' . strtolower($fName) . "\\$fName";
-			$fConfig = jump\ConfigBuilder::build($fClass, $data);
-			if (!class_exists($fClass)) {
-				throw new \UnexpectedValueException("Failed to load class {$fClass}");
-			}
-			$feature = new $fClass($fConfig);
-			$this->features->pushFeature($feature);
-		}
-	}
 
 	/**
 	 * {@inheritdoc}
@@ -77,6 +61,21 @@ final class Jump implements plex\RenderableInterface
 		return '';
 	}
 
+	public function raw_render()
+	{
+		$out = "//START Janrain JUMP\n";
+		foreach ($this->getCssHrefs() as $href) {
+			$out .= "<link type='text/css' rel='stylesheet' href='{$href}'/>\n";
+		}
+		$out .= "<style type='text/css'>\n{$this->getCss()}\n</style>\n";
+		foreach ($this->getHeadJsSrcs as $src) {
+			$out .= "<script type='text/javascript' src='{$src}'></script>\n";
+		}
+		$out .= "<script type='text/javascript><![CDATA[\n{$this->getStartheadJs()}\n{$this->getSettingsHeadJs()}\n{$this->getEndHeadJs()}\n]]></script>";
+		$out .= "//END Janrain JUMP\n";
+		return $out;
+	}
+
 	public function getFeatures()
 	{
 		return $this->features;
@@ -87,11 +86,31 @@ final class Jump implements plex\RenderableInterface
 		return $this->features->getFeature($name);
 	}
 
+	public function init($data)
+	{
+		$coreConfig = jump\ConfigBuilder::build('janrain\\plex\\Core', $data);
+		$core = new plex\Core($coreConfig);
+		$this->features->pushFeature($core);
+		foreach ($data['features'] as $fName) {
+			$fClass = '\\janrain\\plex\\' . strtolower($fName) . "\\$fName";
+			$fConfig = jump\ConfigBuilder::build($fClass, $data);
+			if (!class_exists($fClass)) {
+				throw new \UnexpectedValueException("Failed to load class {$fClass}");
+			}
+			$feature = new $fClass($fConfig);
+			$this->features->pushFeature($feature);
+		}
+	}
+
+	private function __construct()
+	{
+		$this->features = new plex\FeatureStack();
+	}
 	private static $instance;
-	public static function getInstance($data)
+	public static function getInstance()
 	{
 		if (empty(self::$instance)) {
-			self::$instance = new self($data);
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
