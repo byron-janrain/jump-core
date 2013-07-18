6dd8b3e132b0a1e2fd5be99f9349e6d9c02b9da4
Byron Kam <byron@janrain.com>
2013-07-18 15:16:30 -0700

    [DEV] Whitespace cleanup

--- lib/janrain/jump/User.php
+++ lib/janrain/jump/User.php
@@ -4,148 +4,123 @@ namespace janrain\jump;
 /**
  * The fundamental unit of data for Janrain.  The user.
  */
-class User implements \ArrayAccess
+class User implements Mappable
 {
-	public function offsetSet($offset, $value)
-	{
-		if (!$this->offsetExists($offset)) {
-			throw new \InvalidArgumentException('Offset does not exist!');
-		}
-		if (0 === strpos($offset, '/')) {
-			$tokens = array_reverse(preg_split('/[\/\#]/', $offset, -1, PREG_SPLIT_NO_EMPTY));
-			$firstToken = array_pop($tokens);
-			if (!isset($this->$firstToken)) {
-				#sanity check, immediatly fail if the root attribute doesn't exist.
-				return;
-			}
-			$this->attributePathSet($value, $this->$firstToken, $tokens);
-		} else {
-			$this->$offset = $value;
-		}
-	}
+    public function offsetSet($offset, $value)
+    {
+        if (!$this->offsetExists($offset)) {
+            throw new \InvalidArgumentException('Offset does not exist!');
+        }
+        if (0 === strpos($offset, '/')) {
+            $tokens = array_reverse(preg_split('/[\/\#]/', $offset, -1, PREG_SPLIT_NO_EMPTY));
+            $firstToken = array_pop($tokens);
+            if (!isset($this->$firstToken)) {
+                #sanity check, immediatly fail if the root attribute doesn't exist.
+                return;
+            }
+            $this->attributePathSet($value, $this->$firstToken, $tokens);
+        } else {
+            $this->$offset = $value;
+        }
+    }
 
-	private function attributePathSet($newValue, &$next, &$tokens)
-	{
-		$tok = array_pop($tokens);
-		if (is_numeric($tok)) {
-			$tok = intval($tok) - 1;
-		}
-		if (count($tokens) == 0) {
-			$next[$tok] = $newValue;
-		} else {
-			$this->attributePathSet($newValue, $next[$tok], $tokens);
-		}
-	}
+    private function attributePathSet($newValue, &$next, &$tokens)
+    {
+        $tok = array_pop($tokens);
+        if (is_numeric($tok)) {
+            $tok = intval($tok) - 1;
+        }
+        if (count($tokens) == 0) {
+            $next[$tok] = $newValue;
+        } else {
+            $this->attributePathSet($newValue, $next[$tok], $tokens);
+        }
+    }
 
-	public function offsetGet($offset)
-	{
-		#check for capture attribute path
-		if (0 === strpos($offset, '/')) {
-			#attribute path found, crawl it!
-			return $this->attributePathGet($offset);
-		}
-		#no attribute path, just return the value
-		return isset($this->$offset) ? $this->$offset : null;
-	}
-	public function offsetExists($offset)
-	{
-		if (0 === strpos($offset, '/')) {
-			return !is_null($this->attributePathGet($offset));
-		}
-		return isset($this->$offset);
-	}
-	public function offsetUnset($offset)
-	{
-		#cannot unset janrain schema structure
-	}
+    public function offsetGet($offset)
+    {
+        #check for capture attribute path
+        if (0 === strpos($offset, '/')) {
+            #attribute path found, crawl it!
+            return $this->attributePathGet($offset);
+        }
+        #no attribute path, just return the value
+        return isset($this->$offset) ? $this->$offset : null;
+    }
+    public function offsetExists($offset)
+    {
+        if (0 === strpos($offset, '/')) {
+            return !is_null($this->attributePathGet($offset));
+        }
+        return isset($this->$offset);
+    }
+    public function offsetUnset($offset)
+    {
+        #cannot unset janrain schema structure
+    }
 
-	private function attributePathGet($path)
-	{
-		$tok = strtok($path, "/#");
-		$pointer = $this;
-		while (false !== $tok) {
-			#capture plural references start at 1!  tsk tsk
-			if (is_numeric($tok)) {
-				$tok = intval($tok) - 1;
-			}
-			if (isset($pointer[$tok])) {
-				$pointer = $pointer[$tok];
-			} else {
-				#property doesn't exist for this token path, return null
-				return null;
-			}
-			$tok = strtok("/#");
-		}
-		#$pointer should be pointing at the the correct value at this point
-		return $pointer;
-	}
+    private function attributePathGet($path)
+    {
+        $tok = strtok($path, "/#");
+        $pointer = $this;
+        while (false !== $tok) {
+            #capture plural references start at 1!  tsk tsk
+            if (is_numeric($tok)) {
+                $tok = intval($tok) - 1;
+            }
+            if (isset($pointer[$tok])) {
+                $pointer = $pointer[$tok];
+            } else {
+                #property doesn't exist for this token path, return null
+                return null;
+            }
+            $tok = strtok("/#");
+        }
+        #$pointer should be pointing at the the correct value at this point
+        return $pointer;
+    }
 
-	/**
-	 * Create a new Jump User.
-	 *
-	 * You shouldn't create users from nothing since Capture users have a changing schema.  Instead, we can instantiate them from
-	 * raw data using ::__set_state() or create them with a factory that will pull the schema first and ensure required fields are
-	 * populated ::create()
-	 */
-	protected function __construct()
-	{
-		$this->uuid = strtolower(Api::generateUuid());
-	}
+    /**
+     * Create a new Jump User.
+     *
+     * You shouldn't create users from nothing since Capture users have a changing schema.  Instead, we can instantiate them from
+     * raw data using ::__set_state() or create them with a factory that will pull the schema first and ensure required fields are
+     * populated ::create()
+     */
+    protected function __construct()
+    {
+        $this->uuid = strtolower(Api::generateUuid());
+    }
 
-	/**
-	 * Get the unique identifier for this user.  In the rare, and stupid, event that a customer opts to use a different unique
-	 * identifier for users, this function may be overwritten to return that identifier as a string.
-	 *
-	 * @return string
-	 *   The unique identifier of this JumpUser.  Generally this will be UUID
-	 */
-	public function getId()
-	{
-		return $this->uuid;
-	}
+    /**
+     * Get the unique identifier for this user.  In the rare, and stupid, event that a customer opts to use a different unique
+     * identifier for users, this function may be overwritten to return that identifier as a string.
+     *
+     * @return string
+     *   The unique identifier of this JumpUser.  Generally this will be UUID
+     */
+    public function getId()
+    {
+        return $this->uuid;
+    }
 
-	/**
-	 * At it's core, this is a value object so implement the default accessor. The internal structure is php arrays so you can use array notation
-	 * To get at nested data.
-	 * e.g.
-	 *     $var = $instance->propertyName;
-	 *     $var = $instance->propertyName['nestedKey'][0];
-	 *
-	 * Use complex key notation for json-valid-php-invalid key names
-	 *     $var = $instance->{'property.name'};
-	 *     $var = $instance->{"property\nname"};
-	 *
-	 * @param string property
-	 *   The name of the property you're interested in, or an array path to nested data.
-	 *
-	 * @return mixed|null
-	 *   The value for the given property. If the property doesn't exist or isn't set this returns null.
-	 */
-	/*public function __get($property)
-	{
-		if (isset($this->$property)) {
-			return $this->offsetGet($property);
-		}
-		return null;
-	}*/
-
-	/**
-	 * Enable var_export for instances of this class.  This will be the primary factory method for
-	 * rebuilding User objects from Capture data.
-	 *
-	 * @param Array data
-	 *   This data structure is simply stored internally, to pass data directly from a capture response result
-	 *   simply call json_decode() with the optional "force associative array" option.
-	 */
-	public static function __set_state(array $data)
-	{
-		$instance = unserialize(sprintf('O:%u:"%s":0:{}', strlen(__CLASS__), __CLASS__));
-		foreach ($data as $property => $value) {
-			$instance->$property = $value;
-		}
-		if (!$instance->offsetExists('uuid')) {
-			throw new \Exception("No UUID found!");
-		}
-		return $instance;
-	}
+    /**
+     * Enable var_export for instances of this class.  This will be the primary factory method for
+     * rebuilding User objects from Capture data.
+     *
+     * @param Array data
+     *   This data structure is simply stored internally, to pass data directly from a capture response result
+     *   simply call json_decode() with the optional "force associative array" option.
+     */
+    public static function __set_state(array $data)
+    {
+        $instance = unserialize(sprintf('O:%u:"%s":0:{}', strlen(__CLASS__), __CLASS__));
+        foreach ($data as $property => $value) {
+            $instance->$property = $value;
+        }
+        if (!$instance->offsetExists('uuid')) {
+            throw new \Exception("No UUID found!");
+        }
+        return $instance;
+    }
 }
