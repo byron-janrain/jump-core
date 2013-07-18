ed2d11db4f3f326ad735074c77474ce02ce6c9f1
Byron Kam <byron@janrain.com>
2013-07-18 15:16:49 -0700

    mappable!

---
+++ lib/janrain/jump/Mappable.php
@@ -0,0 +1,10 @@
+<?php
+namespace janrain\jump;
+
+/**
+ * The interface expected by the jump platform for objects that wish to have their data mapped
+ */
+interface Mappable extends \ArrayAccess
+{
+	#
+}
