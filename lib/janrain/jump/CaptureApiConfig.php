2c96ac3d552214aafd53ee71b497efc0b5384b7b
Byron Kam <byron@janrain.com>
2013-07-19 17:31:44 -0700

    grumble

--- lib/janrain/jump/CaptureApiConfig.php
+++ lib/janrain/jump/CaptureApiConfig.php
@@ -3,7 +3,7 @@ namespace janrain\jump;
 
 class CaptureApiConfig extends AbstractConfig
 {
-    public static $REQUIRED_KEYS = array('capture.captureServer', 'capture.clientId', 'capture.clientSecret');
+    public static $REQUIRED_KEYS = array('capture.captureServer', 'capture.clientId', 'capture.client_secret');
 
     public function __construct($data)
     {
