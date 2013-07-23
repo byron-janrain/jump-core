82e4af4679b7e96239a4058d1e2e41b5098acb23
Byron Kam <byron@janrain.com>
2013-07-23 09:34:45 -0700

    tabs to spaces

--- lib/janrain/plex/Core.php
+++ lib/janrain/plex/Core.php
@@ -3,100 +3,100 @@ namespace janrain\plex;
 
 class Core extends AbstractFeature implements RenderableInterface
 {
-	protected $config;
+    protected $config;
 
-	public function __construct(\janrain\plex\CoreConfig $c)
-	{
-		$this->config = $c;
-	}
+    public function __construct(\janrain\plex\CoreConfig $c)
+    {
+        $this->config = $c;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getHeadJsSrcs()
-	{
-		return array(
-			'//d3t53pw1y3gpqf.cloudfront.net/localstorage-polyfill.js',
-			'//d3t53pw1y3gpqf.cloudfront.net/prototype-fix-pre1.7.1.js',
-			);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getHeadJsSrcs()
+    {
+        return array(
+            '//d3t53pw1y3gpqf.cloudfront.net/localstorage-polyfill.js',
+            '//d3t53pw1y3gpqf.cloudfront.net/prototype-fix-pre1.7.1.js',
+            );
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getStartHeadJs()
-	{
-		$out = "(function(){
-			var opts;
-			if (typeof window.janrain !== 'object') {window.janrain = {};}
-			if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
-			opts = window.janrain.settings;
-			if (typeof opts.packages !== 'object') opts.packages = [];
-			if (typeof opts.capture !== 'object') opts.capture = {};
-			opts.plex = {};
-			window.janrain.plex = {
-				do: function (actionName, params) {
-					params.do = actionName;
-					janrain.plex.go(opts.plex.jumpUrl, params);
-				},
-				go: function (url, params) {
-					if ('undefined' !== typeof params) {
-						if ('?' != url.slice(-1,1)) {
-							url += '?';
-						}
-						for (var k in params) {
-							url += '&' + encodeURIComponent(k) + '=' + encodeURIComponent(params[k]);
-						}
-					}
-					window.location.href = url;
-				}
-			}\n";
-		return $out;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getStartHeadJs()
+    {
+        $out = "(function(){
+            var opts;
+            if (typeof window.janrain !== 'object') {window.janrain = {};}
+            if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
+            opts = window.janrain.settings;
+            if (typeof opts.packages !== 'object') opts.packages = [];
+            if (typeof opts.capture !== 'object') opts.capture = {};
+            opts.plex = {};
+            window.janrain.plex = {
+                do: function (actionName, params) {
+                    params.do = actionName;
+                    janrain.plex.go(opts.plex.jumpUrl, params);
+                },
+                go: function (url, params) {
+                    if ('undefined' !== typeof params) {
+                        if ('?' != url.slice(-1,1)) {
+                            url += '?';
+                        }
+                        for (var k in params) {
+                            url += '&' + encodeURIComponent(k) + '=' + encodeURIComponent(params[k]);
+                        }
+                    }
+                    window.location.href = url;
+                }
+            }\n";
+        return $out;
+    }
 
-	public function getSettingsHeadJs()
-	{
-		$out = "opts.plex.jumpUrl = '{$this->config['jumpUrl']}'\n";
-		return $out;
-	}
+    public function getSettingsHeadJs()
+    {
+        $out = "opts.plex.jumpUrl = '{$this->config['jumpUrl']}'\n";
+        return $out;
+    }
 
-	public function getEndHeadJs()
-	{
-		$out = "function isReady() {janrain.ready=true;}
-			if (document.addEventListener) {
-				document.addEventListener('DOMContentLoaded', isReady, false);
-			} else {
-				window.attachEvent('onload', isReady);
-			}
-			var e = document.createElement('script');
-			e.type = 'text/javascript';
-			e.id = 'janrainAuthWidget';
-			e.src = opts.plex.loadJsUrl;
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(e, s);
-			})();\n";
-		return $out;
-	}
+    public function getEndHeadJs()
+    {
+        $out = "function isReady() {janrain.ready=true;}
+            if (document.addEventListener) {
+                document.addEventListener('DOMContentLoaded', isReady, false);
+            } else {
+                window.attachEvent('onload', isReady);
+            }
+            var e = document.createElement('script');
+            e.type = 'text/javascript';
+            e.id = 'janrainAuthWidget';
+            e.src = opts.plex.loadJsUrl;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(e, s);
+            })();\n";
+        return $out;
+    }
 
-	public function getCssHrefs()
-	{
-		$out = array();
-		return $out;
-	}
+    public function getCssHrefs()
+    {
+        $out = array();
+        return $out;
+    }
 
-	public function getCss()
-	{
-		$out = '';
-		return $out;
-	}
+    public function getCss()
+    {
+        $out = '';
+        return $out;
+    }
 
-	public function getHtml()
-	{
-		return '';
-	}
+    public function getHtml()
+    {
+        return '';
+    }
 
-	public function getPriority()
-	{
-		return 0;
-	}
+    public function getPriority()
+    {
+        return 0;
+    }
 }
