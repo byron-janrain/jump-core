ee84193d9dffeb8403ef5e64d1513707ad9cb99f
Byron Kam <byron@janrain.com>
2013-07-25 09:10:25 -0700

    whitespace

--- tests/janrain/JumpTest.php
+++ tests/janrain/JumpTest.php
@@ -6,30 +6,30 @@ use PHPUnit_Framework_TestCase;
 class JumpTest extends PHPUnit_Framework_TestCase
 {
 
-	protected $mockConf;
+    protected $mockConf;
 
-	public function setUp()
-	{
-		$this->mockConf = $this->getMockBuilder('janrain\jump\AbstractConfig')
-			->disableOriginalConstructor()
-			->getMock();
-		$this->mockConf
-			->expects($this->any())
-			->method('getIterator')
-			->will($this->returnValue(new \ArrayIterator()));
-			//->method()->will();
-	}
+    public function setUp()
+    {
+        $this->mockConf = $this->getMockBuilder('janrain\jump\AbstractConfig')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->mockConf
+            ->expects($this->any())
+            ->method('getIterator')
+            ->will($this->returnValue(new \ArrayIterator()));
+            //->method()->will();
+    }
 
-	/**
-	 * @expectedException PHPUnit_Framework_Error
-	 */
-	public function testInitNoConfig()
-	{
-		Jump::getInstance();
-	}
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testInitNoConfig()
+    {
+        Jump::getInstance();
+    }
 
-	public function testInitConfig()
-	{
-		Jump::getInstance(array('jumpUrl' => 'jumpUrl', 'features' => array()));
-	}
+    public function testInitConfig()
+    {
+        Jump::getInstance(array('jumpUrl' => 'jumpUrl', 'features' => array()));
+    }
 }
