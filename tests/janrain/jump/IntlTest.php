d9e51ff83fc1adf9f7d3354a7beab96ad5efbb87
Byron Kam <byron@janrain.com>
2013-07-25 11:14:34 -0700

    whitespace

--- tests/janrain/jump/IntlTest.php
+++ tests/janrain/jump/IntlTest.php
@@ -4,28 +4,28 @@ namespace janrain\jump;
 class IntlTest extends \PHPUnit_Framework_TestCase
 {
 
-	public function testFactoryInit()
-	{
-		$xlate = Intl::createForLang();
-		$this->assertInstanceOf(Intl::class, $xlate);
-	}
+    public function testFactoryInit()
+    {
+        $xlate = Intl::createForLang();
+        $this->assertInstanceOf(Intl::class, $xlate);
+    }
 
-	public function testInitDefaultsToEnUs()
-	{
-		$xlate = Intl::createForLang();
-		$this->assertAttributeEquals('en_us', 'lang', $xlate);
-	}
+    public function testInitDefaultsToEnUs()
+    {
+        $xlate = Intl::createForLang();
+        $this->assertAttributeEquals('en_us', 'lang', $xlate);
+    }
 
-	public function testInitLangSet()
-	{
-		$xlate = Intl::createForLang('fr-ca');
-		$this->assertAttributeEquals('fr-ca', 'lang', $xlate);
-	}
+    public function testInitLangSet()
+    {
+        $xlate = Intl::createForLang('fr-ca');
+        $this->assertAttributeEquals('fr-ca', 'lang', $xlate);
+    }
 
-	public function testTranslate()
-	{
-		$xlate = Intl::createForLang();
-		$translated = $xlate("There is no spoon!");
-		$this->assertEquals("There is no spoon!", $translated);
-	}
+    public function testTranslate()
+    {
+        $xlate = Intl::createForLang();
+        $translated = $xlate("There is no spoon!");
+        $this->assertEquals("There is no spoon!", $translated);
+    }
 }
