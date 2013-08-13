97cc67cf09fb7a17a5612b21bc90a0e8d1ab3bc7
Byron Kam <byron@janrain.com>
2013-08-13 13:19:21 -0700

    php 5.5 to 5.3 downgrade

--- sami_config.php
+++ sami_config.php
@@ -1,9 +1,9 @@
 <?php
 return new \Sami\Sami(
     './src',
-    [
+    array(
         'build_dir' => __DIR__ . '/docs',
         'cache_dir' => __DIR__ . '/docs/cache',
         'default_opened_level' => 10,
         'theme' => 'enhanced'
-    ]);
+    ));
