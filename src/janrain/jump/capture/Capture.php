ee732a1866924bd2581007ca552c390bfd80016a
Byron Kam <byron@janrain.com>
2013-08-13 13:20:24 -0700

    namespace plex actions

--- src/janrain/jump/capture/Capture.php
+++ src/janrain/jump/capture/Capture.php
@@ -74,7 +74,7 @@ class Capture extends AbstractFeature implements Renderable
                 janrain.events.onCaptureLoginSuccess.addHandler(
                     function (result) {
                         console.log(result);
-                        janrain.plex.do('login', {token:result.accessToken, uuid:result.userData.uuid});
+                        janrain.plex.do('capturelogin', {token:result.accessToken, uuid:result.userData.uuid});
                         janrain.capture.ui.modal.close();
                     });
                 janrain.events.onCaptureLoginFailed.addHandler(
@@ -86,7 +86,7 @@ class Capture extends AbstractFeature implements Renderable
                     function (result) {
                         console.log('Registration Success!');
                         console.log(result);
-                        janrain.plex.do('login', {token:result.accessToken, uuid:result.userData.uuid});
+                        janrain.plex.do('capturelogin', {token:result.accessToken, uuid:result.userData.uuid});
                         janrain.capture.ui.modal.close();
                     });
                 janrain.capture.ui.start();
